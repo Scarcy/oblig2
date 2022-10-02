31d5cb2a105fce7275b2f1d515f742c78e1b94c7
Ellefsen <monsoyen@gmail.com>
2022-10-02 20:19:29 +0200

    Ferdig med test for LeapYear (Uten user interaction)

--- oblig2/src/main/java/LeapYear.java
+++ oblig2/src/main/java/LeapYear.java
@@ -1,3 +1,13 @@
 public class LeapYear {
 
+    public boolean isLeapYear(int year){
+        if(year % 4 == 0 && year % 100 != 0){
+            return true;
+        }
+        if(year % 400 == 0){
+            return true;
+        }
+
+        return false;
+    }
 }
