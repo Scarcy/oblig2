d43b368e4359d03a6c5da4aa1d5830e92423e989
Ellefsen <monsoyen@gmail.com>
2022-10-03 17:52:08 +0200

    Gjorde ferdig en funksjonell versjon av mange beskrivende test metoder

--- oblig2/src/main/java/LeapYear.java
+++ oblig2/src/main/java/LeapYear.java
@@ -1,13 +1,14 @@
 public class LeapYear {
 
-    public boolean isLeapYear(int year){
-        if(year % 4 == 0 && year % 100 != 0){
-            return true;
-        }
-        if(year % 400 == 0){
-            return true;
-        }
+    public boolean is_divisible_by_4_but_not_100(int year) {
+        return year % 4 == 0 && year % 100 != 0;
+    }
+    public boolean is_divisible_by_400(int year){
+        return year % 400 == 0;
+    }
 
-        return false;
+    public boolean isLeapYear(int year){
+        return is_divisible_by_4_but_not_100(year) || is_divisible_by_400(year);
+
     }
 }
---
+++ oblig2/src/test/java/is_leap_year.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+public class is_leap_year extends Assertions {
+    private final LeapYear leap = new LeapYear();
+
+    @ParameterizedTest
+    @ValueSource(ints = {2020, 2024, 2028, 2032, 2036, 2040, 1884, 1892})
+    public void is_divisible_by_4_but_not_100(int year) {
+        assertTrue(leap.is_divisible_by_4_but_not_100(year));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2000, 400, 800, 1200, 1600})
+    public void is_divisible_by_400(int year) {
+        assertTrue(leap.is_divisible_by_400(year));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2020, 2024, 2028, 2032, 2036, 2040, 1884, 1892})
+    public void is_leapYear(int year) {
+        assertTrue(leap.isLeapYear(year));
+    }
+}
