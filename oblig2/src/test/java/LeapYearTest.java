322525b39ca3cf576b7f0ceed7b5e60afa1315b9
Ellefsen <monsoyen@gmail.com>
2022-10-03 14:00:11 +0200

    Starter med å prøve å loope testene for å refaktorisere koden

--- oblig2/src/test/java/LeapYearTest.java
+++ oblig2/src/test/java/LeapYearTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
     private final LeapYear leap = new LeapYear();
-
+    private final int[] leap_years = {2020,2024,2028,2032,2036,2040, 1884, 1892};
     @Test
     public void testLeapYear(){
         Assertions.assertTrue(leap.isLeapYear(2020));
@@ -12,6 +12,9 @@ public class LeapYearTest {
         Assertions.assertTrue(leap.isLeapYear(2032));
         Assertions.assertTrue(leap.isLeapYear(2036));
         Assertions.assertTrue(leap.isLeapYear(2040));
+        Assertions.assertTrue(leap.isLeapYear(1884));
+        Assertions.assertTrue(leap.isLeapYear(1892));
+
     }
 
 }
