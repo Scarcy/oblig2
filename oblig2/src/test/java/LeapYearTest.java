f39121e8715dc90680cd5fc7966fd3ce4016463d
Ellefsen <monsoyen@gmail.com>
2022-10-03 17:03:27 +0200

    Finalisert versjon med Try Block (Ikke formaterttest)

--- oblig2/src/test/java/LeapYearTest.java
+++ oblig2/src/test/java/LeapYearTest.java
@@ -1,21 +1,20 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
     private final LeapYear leap = new LeapYear();
-    private static final int[] leap_years = {2021, 2024, 2028, 2032, 2036, 2040, 1884, 1892};
-
+    private final int[] years = {2020,2024,2028,2032,2036,2040, 1884, 1892};
     @Test
-    public void testLeapYear(TestInfo testInfo) throws AssertionError {
-        for (int i : leap_years) {
+    public void testLeapYear(){
+        for (int i : years) {
             try {
                 Assertions.assertTrue(leap.isLeapYear(i));
+                System.out.println(i + " is a leap year!");
             } catch (AssertionError e) {
-                System.out.println(e.getMessage());
-                throw new AssertionError(i);
+                System.out.println(e + "\n" + " This is not a leap year: " + i);
+                throw new AssertionError(e);
             }
-            System.out.println(i);
         }
-
 
     }
 
