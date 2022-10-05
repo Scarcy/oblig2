0f542788983040ae6aa0c1de865a690300a30965
Ellefsen <monsoyen@gmail.com>
2022-10-05 15:48:53 +0200

    Forkortet else return if-test

--- oblig2/src/main/java/LeapYear.java
+++ oblig2/src/main/java/LeapYear.java
@@ -4,12 +4,7 @@ public class LeapYear {
        if(year % 4 == 0 && year % 100 != 0){
            return true;
        }
-       else if(year % 400 == 0){
-           return true;
-       }
-       else {
-           return false;
-       }
+       else return year % 400 == 0;
     }
 
 
