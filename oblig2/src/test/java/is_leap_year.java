f8f40dd479509bb40eb7dee784bef7c92a018428
Ellefsen <monsoyen@gmail.com>
2022-10-19 15:52:36 +0200

    Added Run Test to maven.yml. Also changed is_divisby4 param from 2020 to 2021 so the test should fail

--- oblig2/src/test/java/is_leap_year.java
+++ oblig2/src/test/java/is_leap_year.java
@@ -6,7 +6,7 @@ public class is_leap_year extends Assertions {
     private final LeapYear leap = new LeapYear();
 
     @ParameterizedTest
-    @ValueSource(ints = {2020, 2024, 2028, 2032, 2036, 2040, 1884, 1892})
+    @ValueSource(ints = {2021, 2024, 2028, 2032, 2036, 2040, 1884, 1892})
     public void is_divisible_by_4_but_not_100(int year) {
         assertTrue(leap.isLeapYear(year));
     }
